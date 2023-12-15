680d2f8b3ff322acb848d649df272652e2232c4f
BDubOne <bryan.t.bartell@gmail.com>
2023-12-15 14:46:11 -0600

    entry serializer

--- back-end/dict_entry_app/views.py
+++ back-end/dict_entry_app/views.py
@@ -26,6 +26,10 @@ class GlobalDictionaryList(generics.ListCreateAPIView):
             return super(GlobalDictionaryList, self).create(request, *args, **kwargs)
         except ValidationError as e:
             return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+        
+    def perform_create(self, serializer):
+        
+        serializer.save()
 
 class GlobalDictionaryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = DictionaryEntry.objects.all()
@@ -94,11 +98,40 @@ class PersonalDictionaryDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PersonalDictionaryEntrySerializer
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = "number"
-   
-    # @ratelimit(key=user_or_admin_key, rate='30/h', method="GET", block=True)
-    # @ratelimit(key=user_or_admin_key, rate='5/d', method="GET", block=True)
+
     def get_queryset(self):
         return PersonalDictionaryEntry.objects.filter(student=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': 'Entry updated successfully.',
+                'data': serializer.data
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({
+                'message': 'Failed to update entry.',
+                'errors': serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            'message': 'Entry deleted successfully.'
+        }, status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
 
     
 
