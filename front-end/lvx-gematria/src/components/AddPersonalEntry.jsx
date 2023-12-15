6e2c904165cad15d4db0a39103274976e4aa9695
BDubOne <bryan.t.bartell@gmail.com>
2023-12-15 16:01:40 -0600

    cookies on AddPersonalEntry

--- front-end/lvx-gematria/src/components/AddPersonalEntry.jsx
+++ front-end/lvx-gematria/src/components/AddPersonalEntry.jsx
@@ -20,23 +20,47 @@ function AddEntryForm() {
         setEntryData({ ...entryData, [name]: value });
     };
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+
+    //     const formattedData = {
+    //         student_id: user.id,
+    //         number: parseInt(entryData.number, 10),
+    //         personal_description: entryData.personal_description.split(',').map(item => item.trim()),
+    //         personal_key_words: entryData.personal_key_words.split(',').map(item => item.trim()),
+    //         personal_related_entries: entryData.personal_related_entries.split(',').map(Number)
+            
+    //     };
+
+    //     try {
+    //         const token = localStorage.getItem("userToken");
+    //         API.defaults.headers.common["Authorization"] = `Token ${token}`;
+
+    //         const response = await API.post('dictionary/personal/', formattedData);
+    //         console.log("Entry added:", response.data);
+    //         props.onSuccess();
+    //         setEntryData({ number: '', personal_description: '', personal_key_words: '', personal_related_entries: '' });
+    //         navigate('/lvx-calculator/')
+    //     } catch (error) {
+    //         console.error("Error adding entry:", error);
+    //     }
+    // };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+    
         const formattedData = {
-            student_id: user.id,
+            student_id: user.id, 
             number: parseInt(entryData.number, 10),
             personal_description: entryData.personal_description.split(',').map(item => item.trim()),
             personal_key_words: entryData.personal_key_words.split(',').map(item => item.trim()),
             personal_related_entries: entryData.personal_related_entries.split(',').map(Number)
-            
         };
-
+    
         try {
-            const token = localStorage.getItem("userToken");
-            API.defaults.headers.common["Authorization"] = `Token ${token}`;
-
-            const response = await API.post('dictionary/personal/', formattedData);
+            const response = await API.post('dictionary/personal/', formattedData, {
+                withCredentials: true // Ensure cookies are sent with the request
+            });
             console.log("Entry added:", response.data);
             props.onSuccess();
             setEntryData({ number: '', personal_description: '', personal_key_words: '', personal_related_entries: '' });
