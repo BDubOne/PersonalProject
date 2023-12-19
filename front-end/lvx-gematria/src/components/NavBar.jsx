ddeda1521bea4b9ab33eea18ed5c02d974c5b5c7
BDubOne <bryan.t.bartell@gmail.com>
2023-12-19 08:34:06 -0600

    changed navbar

--- front-end/lvx-gematria/src/components/NavBar.jsx
+++ front-end/lvx-gematria/src/components/NavBar.jsx
@@ -1,52 +1,48 @@
-
-import { Link } from "react-router-dom"
-import { API } from '../utilities/API'
-import { useNavigate } from 'react-router-dom'
-
-import SearchComponent from './SearchComponent'
+import { Link, useNavigate } from "react-router-dom";
+import { API } from '../utilities/API';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import NavLink from 'react-bootstrap/NavLink'; // Import NavLink
 
 function NavBar({ user, setUser }) {
-    const navigate = useNavigate()
-    const logOut= () => {
+    const navigate = useNavigate();
+
+    const logOut = () => {
         localStorage.removeItem("token");
         delete API.defaults.headers.common["Authorization"];
         setUser(null);
         navigate('/');
     };
     
-  return (
-    <Navbar style={{ backgroundColor: 'rgb(241, 189, 126)' }} expand="lg" className="bg-body-tertiary">
-      <Container id="navbar">
-        <Navbar.Brand>LVX-Gematria</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/">Home</Link>
-                {user ? (
-                    <>
-                        <Link to="global-dictionary/">Global Dictionary</Link>
-                        <Link to="personal-dictionary/">Personal Dictionary</Link>
-                        <Link to="lvx-calculator/">LVX Calculator</Link>                       
-                        
-                        <Button onClick = {logOut} variant="danger">
-                            Log Out
-                        </Button>
-                    </>
-                ) : (
-                    <Link to={"register/"}>Log in / Sign up</Link>
-            
-            )}
-          </Nav>
-          {/* <SearchComponent /> */}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar style={{ backgroundColor: 'rgb(241, 189, 126)' }} expand="lg" className="bg-body-tertiary">
+            <Container id="navbar">
+                <Navbar.Brand>LVX-Gematria</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink as={Link} to="/" className="mx-2">Home</NavLink>
+                        {user ? (
+                            <>
+                                <NavLink as={Link} to="/global-dictionary/" className="mx-2">Global Dictionary</NavLink>
+                                <NavLink as={Link} to="/personal-dictionary/" className="mx-2">Personal Dictionary</NavLink>
+                                <NavLink as={Link} to="/lvx-calculator/" className="mx-2">LVX Calculator</NavLink>
+                                <Button onClick={logOut} variant="danger" className="mx-2">
+                                    Log Out
+                                </Button>
+                            </>
+                        ) : (
+                            <NavLink as={Link} to="/register/" className="mx-2">Log in / Sign up</NavLink>
+                        )}
+                    </Nav>
+                    {/* <SearchComponent /> */}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavBar;
